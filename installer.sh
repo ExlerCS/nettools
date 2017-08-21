cb2bd61a36e42a05b92aef6f1182db27aca7c60a
Tom Marshall <notroot@exlercs.com>
2017-08-21 22:56:38 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,39 +1,44 @@
- 
 #!/bin/bash
-
-installScripts()
-{       
- printf 'Downloading PiConnect from GitHub...\n'
- wget -q https://raw.githubusercontent.com/tkmarsh/PiConnect/master/piconnect.sh
+installScript(){       
+ printf "Downloading $1 from GitHub... "
+ eval "wget -q" $3 
  if [ $? -eq 0 ]; then
-    printf 'Installing PiConnect...\n'
-    chmod +x piconnect.sh
-    sudo mv piconnect.sh /usr/bin/piconnect
+    printf "Success\nInstalling $1... "
+      if [ $2 == 'piconnect' ]; then
+  	piconnectAddData
+      fi
+    eval "chmod +x $2.sh"
+    eval "sudo mv $2.sh /usr/bin/$2"
     if [ $? -eq 0 ]; then
-     printf 'Installed PiConnect successfully.\n'
+     printf "Success\nInstalled $1 successfully.\n"
     fi
  else
-    printf 'Error downloading, check network connection.\n'
+    printf 'Fail\nError downloading, check network connection.\n'
  fi
- 
- printf '\nDownloading lsip from GitHub...\n'
- wget -q https://raw.githubusercontent.com/tkmarsh/lsip/master/lsip.sh
- if [ $? -eq 0 ]; then
-    printf 'Installing lsip...\n'
-    chmod +x lsip.sh
-    sudo mv lsip.sh /usr/bin/lsip
-    if [ $? -eq 0 ]; then
-     printf 'Installed lsip successfully.\n'
-    fi    
+ printf "\n"
+}
+
+piconnectAddData(){
+ printf '\nPiConnect configuration:\n'
+ printf 'Default Username to use: '
+ read user
+ sed -i "s/\[default login\]/$user/g" piconnect.sh
+ printf 'Default MAC Address to use: '
+ read mac
+
+ if [[ $mac =~ ^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$ ]]; then
+  sed -i "s/\[default device MAC address\]/$mac/g" piconnect.sh
  else
-    printf 'Error downloading, check network connection.\n'
+  printf 'Invalid MAC address.\n'
+  piconnectAddData
  fi
 }
 
-if sudo -n true 2>/dev/null; then 
-       installScripts
-     else
-       echo "Installation of nettools requires root access, please enter your password:"
-       sudo -v
-       installScripts
-     fi
+if sudo -n false 2>/dev/null; then 
+ echo "Installation of nettools requires root access, please enter your password:"
+ sudo -v
+fi
+printf "\e[4mnettools installer\e[0m\n"
+#installScript "[Display Name]" "[Command Name]" "[Script Link]"
+installScript "PiConnect" "piconnect" "https://raw.githubusercontent.com/tkmarsh/PiConnect/master/piconnect.sh"
+installScript "lsip" "lsip" "https://raw.githubusercontent.com/tkmarsh/lsip/master/lsip.sh"
